4e0bc9761d6b231f0809166e5f6c19def1bcc3bf
고경남 <rhrudska987@naver.com>
2024-09-16 15:59:24 +0900

    build: project setting

---
+++ src/main/java/kr/co/onehunnit/onhunnit/OnhunnitApplication.java
@@ -0,0 +1,13 @@
+package kr.co.onehunnit.onhunnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnhunnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnhunnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/onehunnit/onhunnit/OnhunnitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.onehunnit.onhunnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnhunnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
