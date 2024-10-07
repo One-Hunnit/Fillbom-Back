87504fdef41e2b022569d7108375958d2a3bb991
고경남 <rhrudska987@naver.com>
2024-10-07 22:27:27 +0900

    feat(OH-120): email + provider를 통해 유저 구분 로직 생성
    ##관련 이슈
    https://onehunnit.atlassian.net/browse/OH-120
    
    ##작업 내용
    - 유저 식별 수단 변경
    기존: 유저를 nickname을 통해 식별
    변경: email + provider를 통해 유저를 식별
    - Jwt 토큰 로직 리팩토링
    중복 코드 제거
    토큰 정보에 email과 provider가 포함되게 수정

--- src/main/java/kr/co/onehunnit/onhunnit/service/KakaoSocialService.java
+++ src/main/java/kr/co/onehunnit/onhunnit/service/KakaoSocialService.java
@@ -12,117 +12,71 @@ import java.util.HashMap;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import kr.co.onehunnit.onhunnit.domain.account.web.KakaoProperties;
 import kr.co.onehunnit.onhunnit.config.exception.ApiException;
 import kr.co.onehunnit.onhunnit.config.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class KakaoSocialService {
 
-	private static final String KAKAO_LOGIN_REQUEST_URL = "https://kauth.kakao.com/oauth/token";
-	private static final String KAKAO_USER_INFO_REQUEST_URL = "https://kapi.kakao.com/v2/user/me";
+	private static final String KAKAO_TOKEN_INFO_URL = "https://kauth.kakao.com/oauth/tokeninfo";
 
-	private final KakaoProperties kakaoProperties;
-
-	public String getKakaoAccessToken(String authorizationCode) {
+	public HashMap<String, Object> getKakaoUserInfo(String idToken) {
 		try {
-			HttpURLConnection kakaoServerConnection = getKakaoServerConnectionForAuthorizationCode();
-			sendAuthorizationCodeToKakaoServer(kakaoServerConnection, authorizationCode);
-
+			HttpURLConnection kakaoServerConnection = getKakaoServerConnectionForTokenInfo(idToken);
 			if (kakaoServerConnection.getResponseCode() == 200) {
-				return getKakaoAccessToken(kakaoServerConnection);
-			} else {
-				throw new ApiException(ErrorCode.RESPONSE_CODE_ERROR);
+				return getKakaoTokenInfo(kakaoServerConnection);
 			}
-		} catch (IOException e) {
-			throw new ApiException(ErrorCode.FAILED_TO_RETRIEVE_KAKAO_ACCESS_TOKEN);
-		}
-	}
-
-	public HashMap<String, Object> getKakaoUserInfo(String accessToken) {
-		try {
-			HttpURLConnection kakaoServerConnection = getKakaoServerConnectionForUserInfo(accessToken);
-
-			if (kakaoServerConnection.getResponseCode() == 200) {
-				return getKakaoUserInfo(kakaoServerConnection);
-			} else {
-				throw new ApiException(ErrorCode.RESPONSE_CODE_ERROR);
-			}
+			throw new ApiException(ErrorCode.RESPONSE_CODE_ERROR);
 		} catch (IOException e) {
 			throw new ApiException(ErrorCode.FAILED_TO_RETRIEVE_KAKAO_USER_INFO);
 		}
 	}
 
-	private static HttpURLConnection getKakaoServerConnectionForUserInfo(String accessToken) throws IOException {
-		URL url = new URL(KAKAO_USER_INFO_REQUEST_URL);
+	private static HttpURLConnection getKakaoServerConnectionForTokenInfo(String idToken) throws IOException {
+		URL url = new URL(KAKAO_TOKEN_INFO_URL);
 		HttpURLConnection kakaoServerConnection = (HttpURLConnection)url.openConnection();
 		kakaoServerConnection.setRequestMethod("POST");
-		kakaoServerConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
-		return kakaoServerConnection;
-	}
-
-	private static HttpURLConnection getKakaoServerConnectionForAuthorizationCode() throws IOException {
-		URL kakaoLoginRequestUrl = new URL(KAKAO_LOGIN_REQUEST_URL);
-		HttpURLConnection kakaoServerConnection = (HttpURLConnection)kakaoLoginRequestUrl.openConnection();
-		kakaoServerConnection.setRequestMethod("POST");
+		kakaoServerConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 		kakaoServerConnection.setDoOutput(true);
+
+		try (BufferedWriter bw = new BufferedWriter(
+			new OutputStreamWriter(kakaoServerConnection.getOutputStream(), "UTF-8"))) {
+			String requestBody = "id_token=" + idToken;
+			bw.write(requestBody);
+			bw.flush();
+		}
 		return kakaoServerConnection;
 	}
 
-	private void sendAuthorizationCodeToKakaoServer(HttpURLConnection kakaoServerConnection,
-		String authorizationCode) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(kakaoServerConnection.getOutputStream()));
-
-		StringBuilder requestBody = new StringBuilder();
-		requestBody.append("grant_type=authorization_code");
-		requestBody.append("&client_id=" + kakaoProperties.getClientId());
-		requestBody.append("&client_secret=" + kakaoProperties.getClientSecret());
-		requestBody.append("&redirect_uri=" + kakaoProperties.getRedirectUri());
-		requestBody.append("&code=" + authorizationCode);
-
-		bw.write(requestBody.toString());
-		bw.flush();
-		bw.close();
-	}
-
-	private HashMap<String, Object> getKakaoUserInfo(HttpURLConnection kakaoServerConnection) throws IOException {
+	private HashMap<String, Object> getKakaoTokenInfo(HttpURLConnection kakaoServerConnection) throws IOException {
 		String responseBody = readResponse(kakaoServerConnection);
+		log.info("ResponseBody: {}", responseBody);
 
 		JsonElement jsonElement = JsonParser.parseString(responseBody);
-		JsonObject properties = jsonElement.getAsJsonObject().get("properties").getAsJsonObject();
-		JsonObject kakaoAccount = jsonElement.getAsJsonObject().get("kakao_account").getAsJsonObject();
-
-		String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-		// String email = kakaoAccount.getAsJsonObject().get("email").getAsString();
+		String email = jsonElement.getAsJsonObject().get("email").getAsString();
+		String nickname = jsonElement.getAsJsonObject().get("nickname").getAsString();
+		String picture = jsonElement.getAsJsonObject().get("picture").getAsString();
 
 		HashMap<String, Object> kakaoUserInfo = new HashMap<>();
+		kakaoUserInfo.put("email", email);
 		kakaoUserInfo.put("nickname", nickname);
-		// kakaoUserInfo.put("email", email);
-
+		kakaoUserInfo.put("picture", picture);
 		return kakaoUserInfo;
-	}
-
-	private String getKakaoAccessToken(HttpURLConnection kakaoServerConnection) throws IOException {
-		String responseBody = readResponse(kakaoServerConnection);
-		JsonParser jsonParser = new JsonParser();
-		JsonElement jsonElement = jsonParser.parse(responseBody);
-		return jsonElement.getAsJsonObject().get("access_token").getAsString();
 	}
 
 	private String readResponse(HttpURLConnection kakaoServerConnection) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(kakaoServerConnection.getInputStream()));
 		String responseBodyInput = "";
 		StringBuilder responseBody = new StringBuilder();
-
 		while ((responseBodyInput = br.readLine()) != null) {
 			responseBody.append(responseBodyInput);
 		}
-
 		br.close();
 		return responseBody.toString();
 	}
