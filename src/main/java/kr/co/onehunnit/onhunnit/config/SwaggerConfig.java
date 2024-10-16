18041a2ec82b60763020938a38c90c7087f53341
고경남 <rhrudska987@naver.com>
2024-10-16 09:36:53 +0900

    fix: Swagger COSR 에러 처리
    ## 작업 내용
    - @OpenAPIDefinition 어노테이션을 사용하여 API가 호스팅 되는 서버 목록 정의

--- src/main/java/kr/co/onehunnit/onhunnit/config/SwaggerConfig.java
+++ src/main/java/kr/co/onehunnit/onhunnit/config/SwaggerConfig.java
@@ -3,12 +3,20 @@ package kr.co.onehunnit.onhunnit.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "https://api.fillbom.com", description = "개발 서버"),
+		@Server(url = "http://localhost:8080", description = "로컬 서버")
+	}
+)
 @Configuration
 public class SwaggerConfig {
 
